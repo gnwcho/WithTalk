fc7448e2dec4745f419826f94b5f21045f414f01
gnwcho <gnwcho@gmail.com>
2021-05-29 13:03:49 +0900

    대화방 참여자 Map

--- withTalk/src/main/java/com/withTalk/server/handler/MemberHandler.java
+++ withTalk/src/main/java/com/withTalk/server/handler/MemberHandler.java
@@ -30,6 +30,7 @@ public class MemberHandler extends SimpleChannelInboundHandler<String> {
 	
 	@Override
 	protected void messageReceived(ChannelHandlerContext ctx, String msg) throws Exception {
+		System.out.println("------------------------------------------------------------------------" + msg);
 		JSONObject jsonObj = (JSONObject) parser.parse(msg);
 		String type = (String) jsonObj.get("type");
 
